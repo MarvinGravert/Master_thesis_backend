77801ccae7cc1e32896ac11c1e8b24300c23948d
Marvin Gravert <marvin.gravert@gmail.com>
2021-02-28 23:22:45 +0100

    moved modules around

--- Hololens_Vive_Calibrater/config/const.py
+++ Hololens_Vive_Calibrater/config/const.py
@@ -4,5 +4,8 @@ from decouple import config
 TCP_HOST = config("TCP_HOST", cast=str, default="0.0.0.0")
 TCP_PORT = config("TCP_PORT", cast=int, default=15005)
 
-GRPC_HOST = config("GRPC_HOST", cast=str, default="[::]")
-GRPC_PORT = config("GRPC_PORT", cast=int, default=50051)
+BACKEND_HOST = config("BACKEND_HOST", cast=str, default="[::]")
+BACKEND_PORT = config("BACKEND_PORT", cast=int, default=50051)
+
+POINT_REGISTER_HOST = config("POINT_REGISTER_HOST", cast=str, default="[::]")
+POINT_REGISTER_PORT = config("POINT_REGISTER_PORT", cast=int, default="50052")
--- Hololens_Vive_Calibrater/modules/communication/async_tcp_ip_server.py
+++ Hololens_Vive_Calibrater/modules/communication/async_tcp_ip_server.py
@@ -4,13 +4,16 @@ from asyncio.streams import StreamReader, StreamWriter
 
 from loguru import logger
 
-from config.api_types import VRState
+import numpy as np
 
 
 class TcpIPServer():
     def __init__(self, IP: str, port: int):
         self.IP = IP
         self.port = port
+        # synchronize information (if new batch is being read into this will stop processing on the current batch)
+        self.updated_batch = asyncio.Event()
+        self._current_message_batch = None
 
     async def start(self):
         logger.info(f"Async TCP/IP Server is starting on {self.IP}:{self.port}")
@@ -22,23 +25,29 @@ class TcpIPServer():
             await server.serve_forever()
 
     async def communicate_hololens(self, reader: StreamReader, writer: StreamWriter):
-        # wait to receive a message => shows that the hololens wants to know the state
-        # of the controller
-        # await self.vr_state._initialized.wait()  # wait till  VRObjects are init
-        data = await reader.read(100)
-        message = data.decode()
+        # wait for the hololens to connect and accept its data
 
         # some debugging information
         addr = writer.get_extra_info('peername')
         logger.info(f"Received connection from {addr}")
-        # now just keep communication open while always responding with the latest
-        # controller state upon received request
+        # now just read the data into a data container and cancel the connection up the reception of a double
+        # line break in a single message
+        # synchronize the update using an asyncio.Evetn
+        message_container = list()
+        self.updated_batch.clear()
         while True:
-
             data = await reader.read(100)
             message = data.decode()
             logger.debug(f"received: {message}")
+            message_container.append(message)
+            if message == "\n\n" or message[-2:] == "\n\n":
+                break
             # message should be irrelevant hence
             data = b"s"
             writer.write(data)
             await writer.drain()
+            # we will exit this when the outside connection breaks
+        self._current_message_batch = message_container
+        self.updated_batch.set()
+
+        logger.debug(f"Saved message: {self._current_message_batch}")
