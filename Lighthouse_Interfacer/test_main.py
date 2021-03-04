72d5da7f51bf7b14f731a817241a118bf728708a
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-04 18:21:56 +0100

    adds new functionality to utils

--- Lighthouse_Interfacer/test_main.py
+++ Lighthouse_Interfacer/test_main.py
@@ -1,6 +1,8 @@
 """this main is used to test interactoins and is mainly used with the util functions
 """
 
-from utils.device_discovery import discover_devices
+from utils.device_discovery import (
+    discover_devices, print_controller_full,print_device_information,print_controller_pose
+)
 if __name__ == "__main__":
-    discover_devices()
+    print_controller_pose()
