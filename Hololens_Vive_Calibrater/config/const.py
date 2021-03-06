51bc2bde496c5e4cb049c424d79e3b1ac4f98513
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-06 15:38:13 +0100

    fixes spelling error in the server addresses

--- Hololens_Vive_Calibrater/config/const.py
+++ Hololens_Vive_Calibrater/config/const.py
@@ -4,8 +4,8 @@ from decouple import config
 TCP_HOST = config("TCP_HOST", cast=str, default="0.0.0.0")
 TCP_PORT = config("TCP_PORT", cast=int, default=15005)
 
-BACKEND_HOST = config("BACKEND_HOST", cast=str, default="[::]")
+BACKEND_HOST = config("BACKEND_HOST", cast=str, default="[::1]")
 BACKEND_PORT = config("BACKEND_PORT", cast=int, default=50051)
 
-POINT_REGISTER_HOST = config("POINT_REGISTER_HOST", cast=str, default="[::]")
-POINT_REGISTER_PORT = config("POINT_REGISTER_PORT", cast=int, default="50052")
+POINT_REGISTER_HOST = config("POINT_REGISTER_HOST", cast=str, default="[::1]")
+POINT_REGISTER_PORT = config("POINT_REGISTER_PORT", cast=int, default=50052)
