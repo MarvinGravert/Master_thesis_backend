backend: fixes scipy/vive quaternion
scipy rotation wants scalar last while vive sends scalar
hence we need to adjust the transformation

Hololens_Vive_Calibrater/config/api_types.py
@@ -26,10 +26,11 @@ class ViveTracker(VRObject):
 
     def get_as_hom_matrix(self) -> np.ndarray:
         from scipy.spatial.transform import Rotation as R
-        rot_matrix = R.from_quat(self.loc_rot)
+        w, i, j, k = self.loc_rot
+        rot = R.from_quat([i, j, k, w])  # scipy wants scalar last
         trans_vec = np.array(self.loc_trans).reshape([3, 1])
         hom_matrix = np.hstack([
-            rot_matrix.as_matrix(),
+            rot.as_matrix(),
             trans_vec
         ])  # reshape just to be safe
         hom_matrix = np.vstack([hom_matrix, [0, 0, 0, 1]])

backend_servicer/config/api_types.py
@@ -25,8 +25,8 @@ class VRObject():
         Returns:
             np.ndarray: 4x4 homogenous matrix
         """
-
-        rot = R.from_quat(self.loc_rot)
+        w, i, j, k = self.loc_rot
+        rot = R.from_quat([i, j, k, w])  # scipy wants scalar last)
         rot_matrix = rot.as_matrix()
         hom_matrix = np.hstack([rot_matrix, np.array(self.loc_trans).reshape([3, 1])])
         return np.vstack([hom_matrix, [0, 0, 0, 1]])

backend_servicer/modules/holo_communication/async_tcp_ip_server.py
@@ -98,8 +98,10 @@ class TcpIPServer():
 
         rot_matrix = controller_to_hololens[:3, :3]
         quaternion_rot = R.from_matrix(rot_matrix).as_quat()
+        # vive normally transmits scalar first hence we need to adjust
+        i, j, k, w = quaternion_rot
         position = controller_to_hololens[:3, 3]
-        return [position, quaternion_rot]
+        return [position, np.array([w, i, j, k])]
 
     def _turn_pose_into_string(self, pose: List[np.ndarray]) -> str:
         """turn the received pose into a string