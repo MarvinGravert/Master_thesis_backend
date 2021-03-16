bug fixing averagin and loggign data

Hololens_Vive_Calibrater/modules/point_correspondance/find_point_corresponder.py
@@ -126,7 +126,7 @@ def get_points_real_object(vive_trans: List[float], vive_rot: List[float]) -> np
     # triad sends the position as w i j k (scalar first)
     # rotation wants it scalar last
     logger.debug("Starting points acquisition for vive calibration object")
-    logger.debug(f"The vive position: {vive_trans} \n and the vive rotation:{vive_trans}")
+    logger.debug(f"The vive position: {vive_trans} \n and the vive rotation:{vive_rot}")
     w, i, j, k = vive_rot
     rot_matrix: R = R.from_quat([i, j, k, w])
     hom_matrix = np.hstack([

Hololens_Vive_Calibrater/utils/information_logger.py
@@ -20,7 +20,7 @@ from config.api_types import ViveTracker
 
 class DataLogger():
     def __init__(self):
-        self.file_path = Path(".", "calibration_data",
+        self.file_path = Path(".", "calibration_information",
                               datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
         self.calibration_position: List[float] = None  # list[float]
         self.calibration_rotation: List[float] = None  # i j k w
@@ -56,7 +56,7 @@ class DataLogger():
             file.write("Marix LH->Virtual \n ")
             np.savetxt(file, self.hom_LH_to_virtual,)
             file.write("Reprojection error\n")
-            np.savetxt(file, self.reprojection_error)
+            file.write(f"{self.reprojection_error}\n")
             file.write("Matrix Virtual->Tracker\n")
             np.savetxt(file, self.hom_tracker_to_virtual)
             file.write("POINTS THAT WERE MATCHED\n\n")

backend_servicer/config/api_types.py
@@ -6,7 +6,7 @@ from scipy.spatial.transform import Rotation as R
 import numpy as np
 
 from holoViveCom_pb2 import (
-    HandheldController, LighthouseState, TrackerState, Quaternion, Tracker, CalibrationInfo
+    HandheldController, LighthouseState,  Quaternion, Tracker, CalibrationInfo
 )
 
 
@@ -113,6 +113,14 @@ class ViveController(VRObject):
 
         return button_state
 
+    def get_as_grpc_object(self) -> HandheldController:
+        quat = Quaternion(quat=self.loc_rot)
+        trans = self.loc_trans
+        return HandheldController(ID=self.ID,
+                                  rotation=quat,
+                                  position=trans,
+                                  button_state=self._button_state)
+
 
 class Calibration():
     """representation of the calibration matrix which maps from virtual to tracker
@@ -182,6 +190,7 @@ class VRState():
         self._calibration_tracker_set_event = asyncio.Event()
         self.calibration = Calibration()
         self._status: str = "no_status"
+        self.new_state_subscriber: Dict[str, asyncio.Event] = dict()
 
     @ property
     def holo_tracker(self) -> ViveTracker: