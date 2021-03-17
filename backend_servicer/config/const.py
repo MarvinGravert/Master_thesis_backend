12bd854a600f428d90e25615483c9881be2b48c1
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-17 11:54:05 +0100

    backend: adds com to waypoint_manager

--- backend_servicer/config/const.py
+++ backend_servicer/config/const.py
@@ -6,3 +6,6 @@ TCP_PORT = config("TCP_PORT", cast=int, default=15004)
 
 GRPC_HOST = config("GRPC_HOST", cast=str, default="[::]")
 GRPC_PORT = config("GRPC_PORT", cast=int, default=50051)
+
+WAYPOINT_MANAGER_HOST = config("WAYPOINT_MANAGER_HOST", cast=str, default="[::1]")
+WAYPOINT_MANAGER_PORT = config("WAYPOINT_MANAGER_PORT", cast=int, default=50053)
