869491e2b876b5c3653c5662946511c7b742c75e
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-23 23:40:00 +0100

    backend: name changes
    replaced loc_rot, trans, etc with position and rotation to be clear

--- backend_servicer/api/vr_types.py
+++ backend_servicer/api/vr_types.py
@@ -12,12 +12,11 @@ from holoViveCom_pb2 import (
 
 class VRObject():
     def __init__(self,
-                 ID: str,
-                 location_rotation: List[float],
-                 location_tranlation: List[float]) -> None:
-        self.ID = ID
-        self.loc_rot = location_rotation  # w i j k
-        self.loc_trans = location_tranlation  # x y z
+                 position: List[float],
+                 rotation: List[float]) -> None:
+
+        self.rotation = rotation  # w i j k
+        self.position = position  # x y z
 
     def get_pose_as_hom_matrix(self) -> np.ndarray:
         """returns the current pose (postion+quat) as a 4x4 homogenous matrix
@@ -25,31 +24,29 @@ class VRObject():
         Returns:
             np.ndarray: 4x4 homogenous matrix
         """
-        w, i, j, k = self.loc_rot
+        w, i, j, k = self.rotation
         rot = R.from_quat([i, j, k, w])  # scipy wants scalar last)
         rot_matrix = rot.as_matrix()
-        hom_matrix = np.hstack([rot_matrix, np.array(self.loc_trans).reshape([3, 1])])
+        hom_matrix = np.hstack([rot_matrix, np.array(self.position).reshape([3, 1])])
         return np.vstack([hom_matrix, [0, 0, 0, 1]])
 
 
 class ViveTracker(VRObject):
     def update_state(self, new_data: Tracker):
-        self.loc_rot = new_data.rotation.quat
-        self.loc_trans = new_data.position
+        self.rotation = new_data.rotation.quat
+        self.position = new_data.position
 
     def get_as_grpc_object(self) -> Tracker:
-        quat = Quaternion(quat=self.loc_rot)
-        trans = self.loc_trans
-        return Tracker(ID=self.ID, rotation=quat, position=trans)
+        quat = Quaternion(quat=self.rotation)
+        return Tracker(rotation=quat, position=self.position)
 
 
 class ViveController(VRObject):
     def __init__(self,
-                 ID: str,
-                 location_rotation: List[float],
-                 location_tranlation: List[float],
+                 position: List[float],
+                 rotation: List[float],
                  button_state: Dict[str, str]) -> None:
-        super().__init__(ID, location_rotation, location_tranlation)
+        super().__init__(position=position, rotation=rotation)
         self._last_state_menu_button = False
         self._button_state = self._check_and_adjust_button_states(button_state)
         self._menu_button_pressed = asyncio.Event()
@@ -60,8 +57,8 @@ class ViveController(VRObject):
         Args:
             new_data (HandheldController): grpc object containing the new data
         """
-        self.loc_rot = list(new_data.rotation.quat)
-        self.loc_trans = list(new_data.position)
+        self.rotation = list(new_data.rotation.quat)
+        self.position = list(new_data.position)
         # run some checks on the received button state
         self._button_state = self._check_and_adjust_button_states(new_data.button_state)
 
@@ -99,8 +96,8 @@ class ViveController(VRObject):
         menuButton = self._button_state["menu_button"]
         gripButton = self._button_state["grip_button"]
 
-        s = ",".join([str(i) for i in self.loc_trans])+":"
-        s += ",".join([str(i) for i in self.loc_rot])+":"
+        s = ",".join([str(i) for i in self.position])+":"
+        s += ",".join([str(i) for i in self.rotation])+":"
 
         s += x_trackpad+","+y_trackpad+":"+triggerButton+","+trackpadPressed+","+menuButton+","+gripButton
         return s
@@ -123,9 +120,9 @@ class ViveController(VRObject):
         return x_trackpad+","+y_trackpad+":"+triggerButton+","+trackpadPressed+","+menuButton+","+gripButton
 
     def get_as_grpc_object(self) -> HandheldController:
-        quat = Quaternion(quat=self.loc_rot)
-        trans = self.loc_trans
-        return HandheldController(ID=self.ID,
-                                  rotation=quat,
-                                  position=trans,
-                                  button_state=self._button_state)
+        quat = Quaternion(quat=self.rotation)
+        position = self.position
+        return HandheldController(
+            rotation=quat,
+            position=position,
+            button_state=self._button_state)
