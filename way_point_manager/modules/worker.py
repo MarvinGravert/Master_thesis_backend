506796fb42e59d84f92da916acc23c41861e721f
Marvin Gravert <marvin.gravert@gmail.com>
2021-03-27 15:27:17 +0100

    waypoint: worker implemented

--- way_point_manager/modules/worker.py
+++ way_point_manager/modules/worker.py
@@ -5,125 +5,129 @@ world and hololens world
 it does so utliizing asnyc whereever necessary
 """
 import asyncio
-from typing import List
+from modules.grpc_client import BackendCommunicator
+from typing import List, Union
 
 import numpy as np
 from loguru import logger
+from scipy.spatial.transform import Rotation as R
 
 from config.const import (
     BACKEND_HOST, BACKEND_PORT
 )
+from utils.information_processor import InformationProcessor
+from config.api_types import IncorrectMessageFormat, VRObject, VRState
 
 
-async def worker(queue: asyncio.Queue):
-    # Start server
-    logger.info("Worker has started")
+class WorkerClass():
+    def __init__(self) -> None:
+        self.information_processor = InformationProcessor()
 
-    """
-    ------------------
-    Start the services
-    ------------------
-    """
-    while True:
+    async def worker(self, queue: asyncio.Queue, vr_state: VRState):
+        # Start server
+        logger.info("Worker has started")
+        self._vr_state = vr_state
+        backend_client = BackendCommunicator(server_address=BACKEND_HOST, server_port=BACKEND_PORT)
+        """
+        ------------------
+        Start the services
+        ------------------
+        """
+        while True:
+            task: Union[VRObject, List[str]] = await queue.get()
+            logger.info("New job has arrived. Start processing")
+            # first we can get the data from the trackers
+            tracker_state = await backend_client.get_tracker_pose()
+            logger.debug("received information from trackers, now process information")
+            """
+                ------------------
+                Check which workflow to use.
+                Either treat task as a hololens waypoint or a waypoint set directly via controller
+                ------------------
+            """
+            try:
+                if isinstance(task, list):
+                    logger.debug("waypoint is from hololens=>starting hololens workflow")
+                    waypoint = await self.workflow_hololens_waypoint(hololens_message=task,
+                                                                     tracker_state=tracker_state)
+                elif isinstance(task, VRObject):
+                    logger.debug(
+                        "waypoint is directly set via the controller->Starting the controller workflow")
+                    waypoint = await self.workflow_controller_waypoint(controller=task)
+                else:
+                    logger.error("waypoint didnt fit any workflow")
+                    queue.task_done()
+                    continue
+            except IncorrectMessageFormat:
+                logger.error("Message has the incorrect format. Dicarding job")
+                queue.task_done()
+                continue
 
-        hololens_message: List[str] = await queue.get()
-        logger.info("New job has arrived. Start processing")
-        # now we can get the data from the trackers
-        tracker_state = await backend_client.get_tracker_pose()
-        # now  we can process the response we received from the hololens
-        logger.debug("received information from trackers, now process information")
-        """
-        ------------------
-        work through the messages
-        ------------------
-        """
-        info_processor = InformationProcessor()
-        try:
-            hologram_position, hologram_rotation = await info_processor.process_hololens_data(hololens_message)
+            logger.info(f"The waypoint is:\n {waypoint}")
 
-        except IncorrectMessageFormat:
-            logger.error("Message has the incorrect format. Dicarding job")
-            queue.task_done()
-            continue
-
+    async def workflow_controller_waypoint(self,
+                                           controller: VRObject,
+                                           tracker_state: VRState) -> np.ndarray:
+        logger.debug("start controller waypoint workflow")
         """
         ------------------
-        get the point correspondances
+        get vr object homogenous matrix
         ------------------
         """
-        virtual_cali_points = get_points_virtual_object(
-            unity_trans=hologram_position, unity_rot=hologram_rotation)
-        real_cali_points = get_points_real_object(
-            vive_trans=tracker_state.calibration_tracker.loc_trans,
-            vive_rot=tracker_state.calibration_tracker.loc_rot
-        )
-        """
-        ------------------
-        calculate point registering
-        ------------------
-        """
-        reprojection_error, hom_matrix_LH_to_virtual = await points_register_client.register_points(
-            point_set_1=real_cali_points,
-            point_set_2=virtual_cali_points
-        )
-        logger.info(f"Transformtion-Matrix: LH to virtual center:\n {hom_matrix_LH_to_virtual} ")
-        datalog.hom_LH_to_virtual = hom_matrix_LH_to_virtual
-        """
-        ------------------
-        get tracker transformation
-        ------------------
-        """
-        tracker_hom_matrix = tracker_state.holo_tracker.get_as_hom_matrix()
+        hom_matrix_controller_2_LH = controller.get_pose_as_hom_matrix()
+        waypointmarker = np.array([0, 0, 0.2])  # TODO: Change this
         """
         ------------------
         calculate desired transformation
         ------------------
         """
-        # now e can get the transformatoin from the tracker to the virtual center
-        target_hom_matrix = hom_matrix_LH_to_virtual@tracker_hom_matrix
-        logger.info(
-            f"The transformation matrix:  tracker to virtual is:\n {target_hom_matrix}")
-        datalog.hom_tracker_to_virtual = target_hom_matrix
+        LH_2_robot_matrix = self._vr_state.calibration.LH_2_robot_matrix
+
+        return LH_2_robot_matrix@hom_matrix_controller_2_LH@waypointmarker
+
+    async def workflow_hololens_waypoint(self, hololens_message: List[str],
+                                         tracker_state: VRState) -> np.ndarray:
+        logger.debug("start hololens workflow")
+        hologram_position, hologram_rotation = await self.information_processor.process_hololens_data(hololens_message)
+
         """
         ------------------
-        update the other services about the changed calibration
+        get tracker transformation
         ------------------
         """
-        # await backend_client.update_calibration_info(target_hom_matrix)
-        # await waypoint_manager_client.update_calibration_info(target_hom_matrix)
-        # asyncio.gather(backend_client.update_calibration_info(target_hom_matrix),
-        #                waypoint_manager_client.update_calibration_info(target_hom_matrix))
+        holo_tracker_hom_matrix = tracker_state.holo_tracker.get_pose_as_hom_matrix()
         """
         ------------------
-        Log data
+        calculate desired transformation
         ------------------
         """
-        datalog.hololens_message = hololens_message
-        datalog.calibration_position = hologram_position
-        datalog.calibration_rotation = hologram_rotation
-        datalog.calibration_tracker = ViveTracker(
-            ID="cali", location_rotation=tracker_state.calibration_tracker.loc_rot,
-            location_tranlation=tracker_state.calibration_tracker.loc_trans)
-        datalog.holo_tracker = ViveTracker(
-            ID="holo", location_rotation=tracker_state.calibration_tracker.loc_rot,
-            location_tranlation=tracker_state.holo_tracker.loc_trans)
-        datalog.reprojection_error = reprojection_error
-        datalog.real_points = real_cali_points
-        datalog.virtual_points = virtual_cali_points
-        datalog.calibration_object = str(CALIBRATION_OBJECT)
+        hom_hologram_2_virtual_center = self._transform_unity_quat_into_hom(
+            unity_position=hologram_position,
+            unity_quaternion=hologram_rotation)
+        LH_2_robot_matrix = self._vr_state.calibration.LH_2_robot_matrix
+        LH_2_virtual_center = self._vr_state.calibration.LH_2_virtual_matrix
+        # TODO: include the tracker instead of the direct transform
+        virtual_center_2_robo = LH_2_robot_matrix@np.linalg.inv(
+            LH_2_virtual_center)@hom_hologram_2_virtual_center
+        return virtual_center_2_robo
+
+    def _transform_unity_quat_into_hom(
+            self,
+            unity_position: List[float],
+            unity_quaternion: List[float]) -> np.ndarray:
+        """turns the left handed KOS pose (postion+quaternion) into a right handed
+        homogenous matrix
+
+        Args:
+            unity_position (List[float]): x y z in unity KOS (aka lefthanded)
+            unity_quaternion (List[float]): i j k w in unity KOS (aka lefthanded)
+
+        Returns:
+            np.ndarray: 4x4 righthanded hom matrix
         """
-        ------------------
-        Save the log containing the calibration to file
-        ------------------
-        """
-        logger.debug("Starting writing data log to file")
-        try:
-            datalog.write_to_file()
-        except Exception as e:
-            import traceback
-            logger.error(e)
-            logger.error(traceback.format_exc())
-        finally:
-            # TODO: Save the config into a persistent memory
-            queue.task_done()
-            logger.info("Task done")
+        x, y, z = unity_position
+        i, j, k, w = unity_quaternion
+        rotation_matrix = R.from_quat([-i, -k, -j, w])
+        position_vector = np.array([x, z, y])
+        hom_matrix = np.hstack((rotation_matrix, position_vector.reshape((-1, 1))))
+        return np.vstack((hom_matrix, [0, 0, 0, 1]))
