3e3c0e57ea08b1ac06f032e0d50ea0508fc89acd
Marvin Gravert <marvin.gravert@gmail.com>
2021-07-29 17:30:18 +0200

    adds proper transformation

--- waypoint_manager/config/api_types.py
+++ waypoint_manager/config/api_types.py
@@ -7,10 +7,10 @@ class ServerState():
     def __init__(self):
 
         hom_matrix_lh2robot = """
-        1   0  0 0
-        0   1  0 0
-        0  0  1  0  
-        0 0. 0. 1.      
+         0.97352296  0.01101666 -0.22832374 -140.93940735
+        -0.22823046 -0.00910516 -0.97356457 -2205.47338867
+        -0.01280435  0.99989784 -0.00634975 1181.50012207
+        0 0. 0. 1.     
          """
         self.hom_matrix_LH_Robo = np.ndarray = np.fromstring(hom_matrix_lh2robot,
                                                              dtype=float,
